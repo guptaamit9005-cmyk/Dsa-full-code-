bfe9e9c699830467b9e1cc2cb57013cccefa2c19
Amit kumar gupta <guptaamit9005@gmail.com>
2026-09-08 18:34:46 +0530

    Add files via upload

---
+++ DataTypes/mian.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // variable declaration
+    // ek integer type ka variable banado jiska name age hai 
+    int age;
+
+    // variable intialization
+    // int age =  20;
+
+    // char ch = 'a';
+    char ch2 = 'ab';
+    cout << ch2 << endl;
+
+}
