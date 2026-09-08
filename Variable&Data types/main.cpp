4aee6a502fd5e7a4548c07139ec5599489b9ea35
Amit kumar gupta <guptaamit9005@gmail.com>
2026-09-08 18:29:07 +0530

    Add files via upload

---
+++ Variable&Data types/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // variable declaration
+    int marks;
+    cout << "Enter your marks:"<<endl;
+    cin >> marks;
+    cout << "Your marks:" << marks << endl;
+
+}
---
+++ Variable&Data types/main2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int count = 5;
+    float share = 3.14;
+    char alphabet = 'z';
+    double weight = 55.69887;
+    // boolean
+    bool isMale = true;
+    bool isChild = 1;
+    bool isFeamale = false;
+    bool isGirl = 0;
+    cout << count << endl;
+    cout << share << endl;
+    cout << alphabet << endl;
+    cout << weight << endl;
+    cout << isMale << endl;
+    cout << isChild << endl;
+    cout << isFeamale << endl;
+    cout << isGirl << endl;
+
+
+
+
+}
