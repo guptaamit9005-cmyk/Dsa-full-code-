3bd8599284cf5984142faf3252f7b5dd0d99187e
Amit kumar gupta <guptaamit9005@gmail.com>
2026-09-08 18:26:14 +0530

    Add files via upload

---
+++ index.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout << "Enter ypur age:"<<endl;
+    cin>>age;
+
+    cout << "Your age is:"<< age << endl;
+    cout << "Hello, Duniya"<<endl;
+    return 0;
+}
---
+++ main2.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Love Babbar" << endl;
+    return 0;
+
+}
